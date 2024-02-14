3ab32a3abd9d4455ec9b093836bc50d831926eb6
Doctor Kekana <kganyakekana@gmail.com>
2024-02-15 00:46:10 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports={
-    MongoURI:'mongodb+srv://kganya:!542692kK@@cluster0.umhm2.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI:'mongodb+srv://kganya:542692kK.@cluster0.umhm2.mongodb.net/test?retryWrites=true&w=majority'
 }
